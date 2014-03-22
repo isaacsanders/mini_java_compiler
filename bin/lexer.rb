32dd9fa1020421b2b05047dec4e81398046fde7c
Robbie Cooper <cooperra@rose-hulman.edu>
2014-03-21 20:26:04 -0400

    Change runner to output in the required format

--- bin/lexer.rb
+++ bin/lexer.rb
@@ -1,5 +1,7 @@
 require_relative '../lib/lexer'
 
 File.open(ARGV[0]) do |f|
-  puts Lexer.new(f).get_tokens();
+  Lexer.new(f).get_tokens()[0].map do |token|
+    puts token.class.name[7..-1] +  ", " + token[0]
+  end;
 end
