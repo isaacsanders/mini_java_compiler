d63d54025d549bd1174eed9f53d2745d2711802b
Isaac Sanders <isaac@isaacbfsanders.com>
2014-04-07 02:24:10 -0400

    Added all productions!

--- lib/grammar.rb
+++ lib/grammar.rb
@@ -47,8 +47,111 @@ module Grammar
         close_brace_d
       ],
       [
-
+        open_brace_d,
+        ClassVarDeclList,
+        MethodDeclList,
+        close_brace_d
       ]
+    ],
+    ClassVarDeclList => [
+      [ClassVarDecl, ClassVarDeclList],
+      [:epsilon]
+    ],
+    ClassVarDecl => [
+      [Type, Lexer::ID, semicolon_d]
+    ],
+    MethodDeclList => [
+      [MethodDecl, MethodDeclList],
+      [:epsilon]
+    ],
+    MethodDecl => [
+      public_rw, Type, Lexer::ID,
+      open_paren_d, FormalList, close_paren_d,
+      open_brace_d,
+      StmtList,
+      return_rw, Expr, semicolon_d,
+      close_brace_d
+    ],
+    FormalList => [
+      [Formal, FormalListPrime],
+      [:epsilon]
+    ],
+    FormalListPrime => [
+      [comma_d, Formal, FormalListPrime],
+      [:epsilon]
+    ],
+    Formal => [Type, Lexer::ID],
+    Type => [ [int_rw], [boolean_rw], [Lexer::ID] ],
+    StmtList => [
+      [Stmt, StmtList],
+      [:epsilon]
+    ],
+    Stmt => [
+      [Type, Lexer::ID, equals_o, Expr, semicolon_d],
+      [open_brace_d, StmtList, close_brace_d],
+      [if_rw, open_paren_d, Expr, close_paren_d, Stmt, else_rw, Stmt],
+      [while_rw, open_paren_d, expr, close_paren_d, Stmt],
+      [system_out_println_rw, open_paren_d, Expr, close_paren_d, semicolon_d],
+      [Lexer::ID, equals_o, Expr, semicolon_d]
+    ],
+    Expr => [ [Expr7, ExprPrime] ],
+    ExprPrime => [ [or_o, Expr7, ExprPrime], [:epsilon] ],
+    Expr7 => [ [Expr6, Expr7Prime] ],
+    Expr7Prime => [ [and_o, Expr6, Expr7Prime], [:epsilon] ],
+    Expr6 => [ [Expr5, Expr6Prime] ],
+    Expr6Prime => [
+      [eq_o, Expr5, Expr6Prime],
+      [neq_o, Expr5, Expr6Prime],
+      [:epsilon]
+    ],
+    Expr5 => [ [Expr4, Expr5Prime] ],
+    Expr5Prime => [
+      [lt_o, Expr4, Expr5Prime],
+      [lte_o, Expr4, Expr5Prime],
+      [gte_o, Expr4, Expr5Prime],
+      [gt_o, Expr4, Expr5Prime],
+      [:epsilon]
+    ],
+    Expr4 => [ [Expr3, Expr4Prime] ],
+    Expr4Prime => [
+      [add_o, Expr3, Expr4Prime],
+      [sub_o, Expr3, Expr4Prime],
+      [:epsilon]
+    ],
+    Expr3 => [ [Expr2, Expr3Prime] ],
+    Expr3Prime => [
+      [mult_o, Expr2, Expr3Prime],
+      [div_o, Expr2, Expr3Prime],
+      [:epsilon]
+    ],
+    Expr2 => [ [Expr2Prime, Expr1] ],
+    Expr2Prime => [
+      [bang_o, Expr2Prime, Expr1],
+      [sub_o, Expr2Prime, Expr1],
+      [:epsilon]
+    ],
+    Expr1 => [ [Expr0, Expr1Prime] ],
+    Expr1Prime => [
+      [dot_d, Lexer::ID, open_paren_d, ArgList, close_paren_d, Expr1Prime],
+      [:epsilon]
+    ],
+    ArgList => [
+      [Expr, ArgListPrime],
+      [:epsilon]
+    ],
+    ArgListPrime => [
+      [comma_d, Expr, ArgListPrime],
+      [:epsilon]
+    ],
+    Expr0 => [
+      [Lexer::ID],
+      [this_rw],
+      [Lexer::Integer],
+      [null_rw],
+      [true_rw],
+      [false_rw],
+      [new_rw, Lexer::ID, open_paren_d, close_paren_d],
+      [open_paren_d, Expr, close_paren_d]
     ]
   }
 
