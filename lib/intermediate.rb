1e5c88dd351764ab9a128c52c04a58cefd9f2354
Isaac Sanders <isaac@isaacbfsanders.com>
2014-04-17 23:54:10 -0400

    Add initializers

--- lib/intermediate.rb
+++ lib/intermediate.rb
@@ -1,99 +1,124 @@
-class Program
-  main_class
-  class_list
-end
+module Intermediate
+  class Program
+    def initialize(main_class, class_list)
+      @main_class = main_class
+      @class_list = class_list
+    end
+  end
 
-class Class
-  method_list
-  field_list
-  opt_extends
-end
+  class Class
+    def initialize(method_list, field_list, opt_extends)
+      @method_list, @field_list, @opt_extends = method_list, field_list, opt_extends
+    end
+  end
 
-class Method
-  arg_list
-  return_type
-  procedure
-  return_statement
-end
+  class Method
+    def initialize(arg_list, return_type, procedure, return_statement)
+      @arg_list,
+        @return_type,
+        @procedure,
+        @return_statement = arg_list, return_type, procedure, return_statement
+    end
+  end
 
-class Formal
-  type
-  name
-end
+  class Formal
+    def initialize(type, name)
+      @type, @name = type, name
+    end
+  end
 
-class Procedure
-  statement_list
-end
+  class Procedure
+    def initialize(statement_list)
+      @statement_list = statement_list
+    end
+  end
 
-class Statement
-  
-end
+  class Statement
 
-class InitStatement < Statement
-  type # type type
-  id
-  expr # 'type' type
-end
+  end
 
-class BlockStatement < Statement
-  procedure
-end
+  class InitStatement < Statement
+    def initialize(type, id, expr)
+      @type, @id, @expr = type, id, expr
+    end
+    # type # type type
+    # id
+    # expr # 'type' type
+  end
 
-class IfElseStatement < Statement
-  condition_expr # bool type
-  true_statement
-  false_statement
-end
+  class BlockStatement < Statement
+    procedure
+  end
 
-class WhileStatement < Statement
-  condition_expr # bool type
-  statement
-end
+  class IfElseStatement < Statement
+    condition_expr # bool type
+    true_statement
+    false_statement
+  end
 
-class PrintlnStatement < Statement
-  expr # int type
-end
+  class WhileStatement < Statement
+    condition_expr # bool type
+    statement
+  end
 
-class AssignStatement < Statement
-  id
-  value_exp # type must match id's
-end
+  class PrintlnStatement < Statement
+    expr # int type
+  end
 
-class Expression
-  
-end
+  class AssignStatement < Statement
+    # id
+    # value_exp # type must match id's
+    def initialize(id, value_exp)
+      @id, @value_exp = id, value_exp
+    end
+  end
 
-class InfixExpr < Expression
-  expr_a
-  op
-  expr_b
-end
+  class Expression
 
-class PrefixExpr < Expression
-  op
-  expr
-end
+  end
 
-class MethodCallExpr < Expression
-  expr
-  method_id # must be a member of expr's class
-  arg_list # must fit in method_id's args
-end
+  class InfixExpr < Expression
+    def initialize(lhs, op, rhs)
+      @lhs, @op, @rhs = lhs, op, rhs
+    end
+  end
 
-class InitExpr < Expression
-  class_
-end
+  class PrefixExpr < Expression
+    def initialize(op, expr)
+      @op, @expr = op, expr
+    end
+  end
 
-class IDExpr < Expression
-  name
-end
+  class MethodCallExpr < Expression
+    def initialize(expr, method_id, arg_list)
+      @expr, @method_id, @arg_list = expr, method_id, arg_list
+    end
+    # expr
+    # method_id - must be a member of expr's class
+    # arg_list - must fit in method_id's args
+  end
 
-class ThisExpr < Expression
-end
+  class InitExpr < Expression
+    def initialize(klass)
+      @klass = klass
+    end
+  end
 
-class IntLiteralExpr < Expression
-  value
-end
+  class IDExpr < Expression
+    def initialize(name)
+      @name = name
+    end
+  end
 
-class NullExpr < Expression
+  class ThisExpr < Expression
+  end
+
+  class IntLiteralExpr < Expression
+    def initialize(value)
+      @value = value
+    end
+  end
+
+  class NullExpr < Expression
+  end
 end
