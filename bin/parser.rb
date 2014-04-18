166ba675d981e538c36a99909217dbe151224ce9
Isaac Sanders <isaac@isaacbfsanders.com>
2014-04-17 21:58:55 -0400

    Fixed the bin

--- bin/parser.rb
+++ bin/parser.rb
@@ -5,6 +5,6 @@ require_relative '../lib/parser'
 
 File.open(ARGV[0]) do |f|
   tokens = Lexer.new(f).run.tokens
-  tree = Parser.new(tokens).run.tree
-  tree.print
+  tree = Parser.new(tokens).run.parse_tree
+  tree.print_tree
 end
