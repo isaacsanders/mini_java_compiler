a168af80063249accef6380b9810fbf85a66b6ab
Robbie Cooper <cooperra@rose-hulman.edu>
2014-05-22 06:15:35 -0400

    Fix continue so it doesn't, you know, crash my session.

--- lib/intermediate/break_statement.rb
+++ lib/intermediate/break_statement.rb
@@ -1,7 +1,7 @@
 module Intermediate
   class BreakStatement < Statement
     def to_mips(stack_frame)
-      ["j exit#{loop_stack.last}"]
+      ["j exit#{$loop_stack.last}"]
     end
 
     def check_types(errors)
--- lib/intermediate/continue_statement.rb
+++ lib/intermediate/continue_statement.rb
@@ -1,7 +1,7 @@
 module Intermediate
   class ContinueStatement < Statement
     def to_mips(stack_frame)
-      ["j loop#{loop_stack.last}"]
+      ["j continue#{$loop_stack.last}"]
     end
 
     def check_types(errors)
