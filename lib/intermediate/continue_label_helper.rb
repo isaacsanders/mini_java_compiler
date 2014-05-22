76c74994b970d5e3b63966e525dc33869a23e52b
Robbie Cooper <cooperra@rose-hulman.edu>
2014-05-22 05:24:25 -0400

    Fix ContinueLabelHelper

--- lib/intermediate/continue_label_helper.rb
+++ lib/intermediate/continue_label_helper.rb
@@ -1,7 +1,7 @@
 module Intermediate
   class ContinueLabelHelper < Statement
     def to_mips(stack_frame)
-      ["continue#{$loop_stack.last}:", "noop"]
+      ["continue#{$loop_stack.last}:", "nop"]
     end
 
     def check_types(errors)
