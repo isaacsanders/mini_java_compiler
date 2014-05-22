48042aa23ca04b820cf7c1e93aec77a5a2fb5f8a
Robbie Cooper <cooperra@rose-hulman.edu>
2014-05-22 06:11:43 -0400

    Add continue for all example

---
+++ resources/advanced_feature/examples/ContinueForAll.java
@@ -0,0 +1,9 @@
+class ContinueForAll {
+    public static void main(String[] args) {
+	for (int i = 1; i <= 5; i = i + 1) {
+	    System.out.println(0);
+	    continue;
+	    System.out.println(i);
+	}
+    }
+}
